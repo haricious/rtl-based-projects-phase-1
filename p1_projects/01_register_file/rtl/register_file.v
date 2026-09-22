f2ac70d59d25d8e91e29ae0f8238bcf817e56796
Hari <iamhari1812@gmail.com>
2026-09-22 10:13:54 +0530

    Auto commit 2026-09-22 10:13:54

---
+++ p1_projects/01_register_file/rtl/register_file.v
@@ -0,0 +1,18 @@
+`timescale 1ns / 1ps
+
+module dff(
+    input din, clk,
+    output q, qn
+    );
+    
+    always@(posedge clk) begin
+    
+    
+    end
+
+endmodule
+
+module register_file(
+
+    );
+endmodule
