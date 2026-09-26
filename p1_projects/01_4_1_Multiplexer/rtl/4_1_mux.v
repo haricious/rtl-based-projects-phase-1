f8b3f440c92225ce1d220465c12032de0e381c19
Hari Sankar Saravanan <iamhari1812@gmail.com>
2026-09-26 12:44:06 +0530

    auto: 2026-09-26 12:44:06

--- p1_projects/01_4_1_Multiplexer/rtl/4_1_mux.v
+++ p1_projects/01_4_1_Multiplexer/rtl/4_1_mux.v
@@ -5,7 +5,7 @@ module mux_4x1(
     output y
 );
 
-    assign y = (~s0&~s1&n0)|(~s0&s1&n1)|(s0&~s1%n3)|(s0&s1&n3);
+    assign y = (~s0&~s1&n0)|(~s0&s1&n1)|(s0&~s1&n3)|(s0&s1&n3);
 
 endmodule
 
@@ -15,7 +15,7 @@ module mux_4x1(
     input n0, n1, n2, n3,
     input s0, s1,
     output y
-)
+);
 
     assign y = s1 ? (s0?n3:n2) : (s0?n1:n0);
 
@@ -31,14 +31,14 @@ module mux_4x1(
 
 always @(*) begin
 
-    case(sel) begin
+    case(sel)
 
         2'b00: y = n[0];
         2'b01: y = n[1];
         2'b10: y = n[2];
         2'b11: y = n[3];
 
-    end
+    endcase
 
     default y = 4'b0000;
 
