eadff6ca0b655ff199b180735928aa44648d67a4
Hari Sankar Saravanan <iamhari1812@gmail.com>
2026-09-26 13:04:41 +0530

    auto: 2026-09-26 13:04:41

---
+++ p1_projects/01_4_1_Multiplexer/vivado_file/4_1_MUX/4_1_MUX.srcs/sources_1/new/mux_4x1.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/26/2026 01:04:39 PM
+// Design Name: 
+// Module Name: mux_4x1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mux_4x1(
+
+    );
+endmodule
