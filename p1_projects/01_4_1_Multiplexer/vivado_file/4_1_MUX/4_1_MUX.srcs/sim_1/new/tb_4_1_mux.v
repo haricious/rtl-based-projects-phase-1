54c2f1a47cdbeaef741337a010fd24e7fc11e821
Hari Sankar Saravanan <iamhari1812@gmail.com>
2026-09-26 13:32:38 +0530

    auto: 2026-09-26 13:32:38

--- p1_projects/01_4_1_Multiplexer/vivado_file/4_1_MUX/4_1_MUX.srcs/sim_1/new/tb_4_1_mux.v
+++ p1_projects/01_4_1_Multiplexer/vivado_file/4_1_MUX/4_1_MUX.srcs/sim_1/new/tb_4_1_mux.v
@@ -1,53 +1,40 @@
 `timescale 1ns / 1ps
 
+// 4x1 Multiplexer using behavioural modelling using case
+
 module tb_4_1_mux();
 
-reg n0, n1, n2,n3;
-reg s0, s1;
+reg [3:0]n, [1:0]sel;
 wire y;
 
-mux_4x1 dut (n0,n1,n2,n3,s0,s1,y);
+mux_4x1 dut(n, sel, y);
+initial 
+n=$random;
 
-initial begin
+always @(*) begin
 
-n0=0;
-n1=0;
-n2=0;
-n3=0;
+    n[0]=$random 3'h7;
+    n[1]=$random 3'h7;
+    n[2]=$random 3'h7;
+    n[3]=$random 3'h7
 
-s0=0;
-s1=0;
-#5;
+    #10;
+    sel = 2'b00;
+    #10;
+    sel = 2'b01;
+    #10;
+    sel = 2'b10;
+    #10;
+    sel = 2'b11;
+        
 
-n0=0;
-n1=1;
-n2=0;
-n3=1;
-s0=0;
-s1=1;
-
-
-#5;
-n0=1;
-n1=0;
-n2=1;
-n3=0;
-s0=1;
-s1=0;
-
-#5;
-n0=1;
-n1=1;
-n2=1;
-n3=0;
-s0=1;
-s1=1;
+    end
 
 
 $finish;
 
-end
-
 endmodule
+
+
 
 
