1ae33f247a5af16941bc3afb37302f5fb75f1956
Hari Sankar Saravanan <iamhari1812@gmail.com>
2026-09-26 13:29:30 +0530

    auto: sync 2026-09-26 13:29:30

--- p1_projects/01_4_1_Multiplexer/vivado_file/4_1_MUX/4_1_MUX.srcs/sources_1/new/mux_4x1.v
+++ p1_projects/01_4_1_Multiplexer/vivado_file/4_1_MUX/4_1_MUX.srcs/sources_1/new/mux_4x1.v
@@ -1,14 +1,11 @@
 `timescale 1ns / 1ps
-// 4X1 Multiplexer using boolean expressions
+
 module mux_4x1(
-    input n0,n1,n2,n3,
-    input s0,s1,
+    input n0, n1, n2, n3,
+    input s0, s1,
     output y
 );
 
-    assign y = (~s1 & ~s0 & n0) |
-           (~s1 &  s0 & n1) |
-           ( s1 & ~s0 & n2) |
-           ( s1 &  s0 & n3);
+    assign y = s1 ? (s0?n3:n2) : (s0?n1:n0);
 
 endmodule
