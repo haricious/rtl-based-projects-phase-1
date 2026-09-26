373ec71596bdd643fb0c4dbd50040878fc489a35
Hari Sankar Saravanan <iamhari1812@gmail.com>
2026-09-26 11:00:36 +0530

    auto: 2026-09-26 11:00:36

---
+++ p1_projects/01_4_1_Multiplexer/rtl/register_file.v
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+
+module dff(
+    input din, clk,
+    output reg q, qn
+    );
+    
+    always@(posedge clk) begin
+
+    
+    end
+
+endmodule
+
+module register_file(
+    input data_in,clk, 
+    output reg dout
+    );
+    
+    ff1 dff(data_in, clk, dout, );
+endmodule
