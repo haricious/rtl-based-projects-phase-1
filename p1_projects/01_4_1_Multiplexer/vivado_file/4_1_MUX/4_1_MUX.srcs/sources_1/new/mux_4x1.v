bdb1f4ebd11573aa094792e07f374152008c99cf
Hari Sankar Saravanan <iamhari1812@gmail.com>
2026-09-26 13:35:09 +0530

    auto: sync 2026-09-26 13:35:09

--- p1_projects/01_4_1_Multiplexer/vivado_file/4_1_MUX/4_1_MUX.srcs/sources_1/new/mux_4x1.v
+++ p1_projects/01_4_1_Multiplexer/vivado_file/4_1_MUX/4_1_MUX.srcs/sources_1/new/mux_4x1.v
@@ -1,11 +1,34 @@
 `timescale 1ns / 1ps
 
-module mux_4x1(
-    input n0, n1, n2, n3,
-    input s0, s1,
-    output y
-);
+// 4x1 Multiplexer using behavioural modelling using case
 
-    assign y = s1 ? (s0?n3:n2) : (s0?n1:n0);
+module tb_4_1_mux();
+
+reg [3:0]n;
+reg [1:0]sel;
+wire y;
+
+mux_4x1 dut(n, sel, y);
+initial 
+n=$random;
+
+always @(*) begin
+
+    n[0]=$random & 3'h7;
+    n[1]=$random & 3'h7;
+    n[2]=$random & 3'h7;
+    n[3]=$random & 3'h7;
+
+    #10;
+    sel = 2'b00;
+    #10;
+    sel = 2'b01;
+    #10;
+    sel = 2'b10;
+    #10;
+    sel = 2'b11;
+        
+    $finish;
+    end
 
 endmodule
