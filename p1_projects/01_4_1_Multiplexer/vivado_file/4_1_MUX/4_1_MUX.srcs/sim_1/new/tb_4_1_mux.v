06299a17e11cb1131b85eb20c46bf843a64a6e0f
Hari Sankar Saravanan <iamhari1812@gmail.com>
2026-09-26 13:05:12 +0530

    auto: sync 2026-09-26 13:05:12

---
+++ p1_projects/01_4_1_Multiplexer/vivado_file/4_1_MUX/4_1_MUX.srcs/sim_1/new/tb_4_1_mux.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/26/2026 01:05:12 PM
+// Design Name: 
+// Module Name: tb_4_1_mux
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_4_1_mux(
+
+    );
+endmodule
