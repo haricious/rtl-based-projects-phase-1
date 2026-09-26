adfc6e3fd96e33243bf6f2d6a55aeac9ce16708e
Hari Sankar Saravanan <iamhari1812@gmail.com>
2026-09-26 13:06:10 +0530

    auto: sync 2026-09-26 13:06:10

--- p1_projects/01_4_1_Multiplexer/vivado_file/4_1_MUX/4_1_MUX.srcs/sim_1/new/tb_4_1_mux.v
+++ p1_projects/01_4_1_Multiplexer/vivado_file/4_1_MUX/4_1_MUX.srcs/sim_1/new/tb_4_1_mux.v
@@ -1,26 +1,56 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09/26/2026 01:05:12 PM
-// Design Name: 
-// Module Name: tb_4_1_mux
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
+module tb_4_1_mux();
+
+reg n0, n1, n2,n3;
+reg s0, s1;
+wire y;
+
+mux4_4x1 dut (n0,n1,n2,n3,s0,s1,y);
+
+initial begin
+
+n0=0;
+n1=0;
+n2=0;
+n3=0;
+
+s0=0;
+s1=0;
+#5;
+
+n0=0;
+n1=1;
+n2=0;
+n3=1;
+s0=0;
+s1=1;
 
 
-module tb_4_1_mux(
+#5;
+n0=1;
+n1=0;
+n2=1;
+n3=0;
+s0=1;
+s1=0;
 
-    );
+#5;
+n0=1;
+n1=1;
+n2=1;
+n3=0;
+s0=1;
+s1=1;
+
+$finish
+
+if (y !== 0)
+    $display("FAIL: expected 0, got %b", y);
+else
+    $display("PASS");
+
+
+end
+
 endmodule
