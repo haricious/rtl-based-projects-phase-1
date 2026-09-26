46de058e47bb6cc11e4c0bd86c8c844731be8b12
Hari Sankar Saravanan <iamhari1812@gmail.com>
2026-09-26 13:17:26 +0530

    auto: sync 2026-09-26 13:17:26

--- p1_projects/01_4_1_Multiplexer/tb/tv_4_1_mux.v
+++ p1_projects/01_4_1_Multiplexer/tb/tv_4_1_mux.v
@@ -6,7 +6,7 @@ reg n0, n1, n2,n3;
 reg s0, s1;
 wire y;
 
-mux4_4x1 dut (n0,n1,n2,n3,s0,s1,y);
+mux_4x1 dut (n0,n1,n2,n3,s0,s1,y);
 
 initial begin
 
@@ -43,15 +43,12 @@ n3=0;
 s0=1;
 s1=1;
 
+
 $finish
 
-if (y !== 0)
-    $display("FAIL: expected 0, got %b", y);
-else
-    $display("PASS");
-
-
 end
+
+endmodule
 
 
 // 4x1 Multiplexer using conditional logic ternary operator
@@ -61,7 +58,7 @@ reg n0, n1, n2,n3;
 reg s0, s1;
 wire y;
 
-mux4_4x1 dut (n0,n1,n2,n3,s0,s1,y);
+mux_4x1 dut (n0,n1,n2,n3,s0,s1,y);
 
 initial begin
 
@@ -98,19 +95,48 @@ n3=0;
 s0=1;
 s1=1;
 
+
+
 $finish
-
-if (y !== 0)
-    $display("FAIL: expected 0, got %b", y);
-else
-    $display("PASS");
-
-
 end
+
+endmodule
 
 
 // 4x1 Multiplexer using behavioural modelling using case
 
+module tb_4_1_mux();
+
+reg [3:0]n, [1:0]sel;
+wire y;
+
+mux_4x1 dut(n, sel, y);
+initial 
+n=$random;
+
+always @(*) begin
+
+    n[0]=$random 3'h7;
+    n[1]=$random 3'h7;
+    n[2]=$random 3'h7;
+    n[3]=$random 3'h7
+
+    #10;
+    sel = 2'b00;
+    #10;
+    sel = 2'b01;
+    #10;
+    sel = 2'b10;
+    #10;
+    sel = 2'b11;
+        
+
+    end
+
+
+$finish
+
+endmodule
 
 
 
