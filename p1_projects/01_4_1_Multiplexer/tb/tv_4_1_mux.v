3cf69d053fa84c106232276c4219e785facfba30
Hari Sankar Saravanan <iamhari1812@gmail.com>
2026-09-26 12:57:57 +0530

    auto: sync 2026-09-26 12:57:57

--- p1_projects/01_4_1_Multiplexer/tb/tv_4_1_mux.v
+++ p1_projects/01_4_1_Multiplexer/tb/tv_4_1_mux.v
@@ -55,7 +55,58 @@ end
 
 
 // 4x1 Multiplexer using conditional logic ternary operator
+module tb_4_1_mux();
 
+reg n0, n1, n2,n3;
+reg s0, s1;
+wire y;
+
+mux4_4x1 dut (n0,n1,n2,n3,s0,s1,y);
+
+initial begin
+
+n0=0;
+n1=0;
+n2=0;
+n3=0;
+
+s0=0;
+s1=0;
+#5;
+
+n0=0;
+n1=1;
+n2=0;
+n3=1;
+s0=0;
+s1=1;
+
+
+#5;
+n0=1;
+n1=0;
+n2=1;
+n3=0;
+s0=1;
+s1=0;
+
+#5;
+n0=1;
+n1=1;
+n2=1;
+n3=0;
+s0=1;
+s1=1;
+
+$finish
+
+if (y !== 0)
+    $display("FAIL: expected 0, got %b", y);
+else
+    $display("PASS");
+
+
+end
 
 
 // 4x1 Multiplexer using behavioural modelling using case
