19daac14058f237bad429f2a4c988dcfb72d74d8
Hari Sankar Saravanan <iamhari1812@gmail.com>
2026-09-26 13:29:51 +0530

    auto: sync 2026-09-26 13:29:51

--- p1_projects/01_4_1_Multiplexer/vivado_file/4_1_MUX/4_1_MUX.srcs/sim_1/new/tb_4_1_mux.v
+++ p1_projects/01_4_1_Multiplexer/vivado_file/4_1_MUX/4_1_MUX.srcs/sim_1/new/tb_4_1_mux.v
@@ -1,11 +1,53 @@
 `timescale 1ns / 1ps
 
-module mux_4x1(
-    input n0, n1, n2, n3,
-    input s0, s1,
-    output y
-);
+module tb_4_1_mux();
 
-    assign y = s1 ? (s0?n3:n2) : (s0?n1:n0);
+reg n0, n1, n2,n3;
+reg s0, s1;
+wire y;
+
+mux_4x1 dut (n0,n1,n2,n3,s0,s1,y);
+
+initial begin
+
+n0=0;
+n1=0;
+n2=0;
+n3=0;
+
+s0=0;
+s1=0;
+#5;
+
+n0=0;
+n1=1;
+n2=0;
+n3=1;
+s0=0;
+s1=1;
+
+
+#5;
+n0=1;
+n1=0;
+n2=1;
+n3=0;
+s0=1;
+s1=0;
+
+#5;
+n0=1;
+n1=1;
+n2=1;
+n3=0;
+s0=1;
+s1=1;
+
+
+$finish;
+
+end
 
 endmodule
+
+
